6a37dec94369f625e59203c0f2f10da2b51d39f2
mohammed.bouna <mohammed.bouna07@gmail.com>
2022-11-09 14:19:10 +0100

    image recipe

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,8 +23,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-#from core import views as core_views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
